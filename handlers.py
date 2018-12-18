a56e8ab2d80db4d9586949e9fe58d45e2d14a40f
jack-zheng <jack.zheng@linecorp.com>
2018-12-18 15:59:27 +0800

    fix groupid shortcut

--- handlers.py
+++ handlers.py
@@ -92,7 +92,7 @@ def text_message_handler(event):
     if BOT_NAME.lower() == event.message.text.lower():
         show_menu_handler()
     elif event.message.text.lower() == "groupid":
-        line_bot_api.reply_message(event.reply_token, event.source.groupId)
+        line_bot_api.reply_message(event.reply_token, event.source["groupId"])
 
 
 
