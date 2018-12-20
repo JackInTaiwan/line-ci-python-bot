e284db64b104eee7a67a8de73db367560cedf70b
jack-zheng <jack.zheng@linecorp.com>
2018-12-20 17:25:20 +0800

    update projects list

--- drone-plugin/README.md
+++ drone-plugin/README.md
@@ -3,6 +3,6 @@
 ```
 pipeline:
     line-flex-notify:
-        image: wei840222/drone-line-flex-notify-bot-plugin
+        image: wei840222/drone-line-flex-notify-bot-plugin:v2
         api_url: bot api url
 ```
--- handlers.py
+++ handlers.py
@@ -95,7 +95,10 @@ def ci_post():
     users = list(repo["users"])
     group_id = [(item["name"], item["group_id"]) for item in env["projects"] if item["name"] == drone_repo_name][0][1]
     flex_message = FlexSendMessage(alt_text="{} repo 有最新更動！".format(drone_repo_name), contents=BubbleContainer.new_from_json_dict(dict_template))
-    text_message = TextMessage( text="請 *@{}*  負責審 PR !".format(random.choice(users)["user_name"]))
+    try:
+        text_message = TextMessage(text="請 *@{}*  負責審 PR !".format(random.choice(users)["user_name"]))
+    except:
+        text_message = TextMessage(text="孤單寂寞，沒有人關心我，嗚...")
     line_bot_api.push_message(group_id, [flex_message, text_message])
 
     return 'OK'
