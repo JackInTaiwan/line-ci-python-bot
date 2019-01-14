08b3aaa9887f6e755fa9719508d21daae2a5eb6f
jack-zheng <jack.zheng@linecorp.com>
2019-01-14 14:22:32 +0800

    print request

--- handlers.py
+++ handlers.py
@@ -48,6 +48,7 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+    print("Request body: " + body)
 
     # handle webhook body
     try:
